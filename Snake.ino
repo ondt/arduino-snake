15562da0cf8c11eacea5cb64ee68dee5b775fffc
Ondrej Telka <ondrejtelka@gmail.com>
2020-09-03 16:27:39 +0200

    Add GPL header

--- Snake.ino
+++ Snake.ino
@@ -1,3 +1,19 @@
+/* 
+ * Copyright (c) 2018 Ondrej Telka. (https://ondrej.xyz/)
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "LedControl.h" // LedControl library is used for controlling a LED matrix. Find it using Library Manager or download zip here: https://github.com/wayoda/LedControl
 
 
